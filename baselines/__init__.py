27829c7e8dedf3327022af8c7eac9d7d60da569c
Daniel Grimmer <52511587+DanielGrimmer@users.noreply.github.com>
2026-05-02 15:38:22 -0400

    Update __init__.py

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,2 +1,3 @@
 #Empty File
-#NoisyAdam is a baseline comparison optimizer used in benchmarking. It is not part of the Adam-DLS package and is not intended for production use.
+#NoisyAdam is a baseline comparison optimizer used in benchmarking. It is not part of the Adam-DLS package and is not intended for production use
+from .noisy_adam import NoisyAdam
