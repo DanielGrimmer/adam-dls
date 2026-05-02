b8b8b4b4608d5873a242576832786ef68d6b94c6
Daniel Grimmer <52511587+DanielGrimmer@users.noreply.github.com>
2026-05-02 16:13:34 -0400

    Update comments in __init__.py for NoisyAdam
    Added additional information about NoisyAdam as a benchmark optimizer.

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,3 +1,4 @@
-#Empty File
-#NoisyAdam is a baseline comparison optimizer used in benchmarking. It is not part of the Adam-DLS package and is not intended for production use
+# Empty File
+# NoisyAdam is a baseline comparison optimizer used in benchmarking. It is not part of the Adam-DLS package and is not intended for production use
+# It is just a naive isotropic Gaussian noise on top of Adam. It is used as a benchmark foil for Adam-DLS
 from .noisy_adam import NoisyAdam
